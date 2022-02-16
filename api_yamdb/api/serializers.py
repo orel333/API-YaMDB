5cad163ee6cdbeb0cb415bdbad45ec7f59657dc7
Егор Марецкий <emarpoint@gmail.com>
2022-02-16 18:42:45 +0300

    First comit

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -60,25 +60,8 @@ class ReviewSerializer(serializers.ModelSerializer):
         read_only=True
     )
 
-    def validate_score(self, value):
-        if 0 > value > 10:
-            raise serializers.ValidationError('Оценка по 10-бальной шкале!')
-        return value
-
-    def validate(self, data):
-        request = self.context['request']
-        author = request.user
-        title_id = self.context.get('view').kwargs.get('title_id')
-        title = get_object_or_404(Title, pk=title_id)
-        if (
-            request.method == 'POST'
-            and Review.objects.filter(title=title, author=author).exists()
-        ):
-            raise ValidationError('Может существовать только один отзыв!')
-        return data
-
     class Meta:
-        fields = '__all__'
+        fields = ('id', 'title', 'text','score', 'author', 'pub_date')
         model = Review
 
 
@@ -93,5 +76,5 @@ class CommentSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = '__all__'
+        fields = ('id', 'author', 'review', 'text', 'created', 'pub_date')
         model = Comment
