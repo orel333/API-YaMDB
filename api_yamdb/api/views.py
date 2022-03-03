7ff8191a7b237f11cb64346ac37d49e1a07db32e
orel333 <orel333@mail.ru>
2022-03-03 21:48:13 +0300

    finalized

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -238,26 +238,35 @@ class ReviewViewSet(viewsets.ModelViewSet):
     """Пользователи оставляют к произведениям текстовые отзывы."""
     serializer_class = ReviewSerializer
     permission_classes = (IsAdminModeratorUserPermission,)
+
     def get_queryset(self):
         if getattr(self, 'swagger_fake_view', False):
             return Title.objects.none()
-        review = get_object_or_404(Title, id=self.kwargs.get('title_id'))
-        return review.reviews.all()
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        return title.reviews.all()
+
     def perform_create(self, serializer):
         title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
         serializer.save(author=self.request.user, title=title)
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     """Пользователи оставляют коментарии к отзывам."""
     serializer_class = CommentSerializer
     permission_classes = (IsAdminModeratorUserPermission,)
+
     def get_queryset(self):
         review = get_object_or_404(
-            Review, id=self.kwargs.get('review_id'),
+            Review,
+            id=self.kwargs.get('review_id'),
             title__id=self.kwargs.get('title_id')
         )
         return review.comments.all()
+
     def perform_create(self, serializer):
-        review = get_object_or_404(Review, title_id=self.kwargs.get('title_id'),
-                                   id=self.kwargs.get('review_id'))
+        review = get_object_or_404(
+            Review,
+            title_id=self.kwargs.get('title_id'),
+            id=self.kwargs.get('review_id')
+        )
         serializer.save(author=self.request.user, review=review)
